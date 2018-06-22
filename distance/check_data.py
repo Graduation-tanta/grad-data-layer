15214714ac4ca7ac9fd244480388febb287b0e66
AmiraElbaradei <amiraelbaradei5@gmail.com>
2018-06-22 11:08:17 +0200

    add distance code with edit in doc_db file

---
+++ distance/check_data.py
@@ -0,0 +1,25 @@
+"""A script to visually inspect generated data."""
+
+import argparse
+import json
+from termcolor import colored
+
+parser = argparse.ArgumentParser()
+parser.add_argument('file', type=str)
+args = parser.parse_args()
+
+with open(args.file) as f:
+    lines = f.readlines()
+    for line in lines:
+        data = json.loads(line)
+        question = ' '.join(data['question'])
+        start, end = data['answers'][0]
+        doc = data['document']
+        pre = ' '.join(doc[:start])
+        ans = colored(' '.join(doc[start: end + 1]), 'red', attrs=['bold'])
+        post = ' '.join(doc[end + 1:])
+        print('-' * 50)
+        print('Question: %s' % question)
+        print('')
+        print('Document: %s' % (' '.join([pre, ans, post])))
+        input()
